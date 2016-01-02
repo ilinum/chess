9815f41c8e56ee7b4536ef4c0e6bda706d181179
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2016-01-02 13:48:06 +0300

    Make GameRunner not depend Swing, rename Main to JSwingMain

--- src/main/java/me/ilinskiy/chess/JSwingMain.java
+++ src/main/java/me/ilinskiy/chess/JSwingMain.java
@@ -3,18 +3,20 @@ package me.ilinskiy.chess;
 import me.ilinskiy.chess.chessBoard.PieceColor;
 import me.ilinskiy.chess.game.GameRunner;
 import me.ilinskiy.chess.game.Player;
+import me.ilinskiy.chess.ui.JSwingChessPainter;
 import me.ilinskiy.chess.ui.JSwingUserPlayer;
 
 @SuppressWarnings("WeakerAccess")
-public class Main {
+public class JSwingMain {
 
     public static void main(String[] args) {
         Player p1 = new JSwingUserPlayer(PieceColor.White);
         Player p2 = new JSwingUserPlayer(PieceColor.Black);
-        GameRunner.askTimeOut();
+        GameRunner gameRunner = new GameRunner(new JSwingChessPainter());
+        gameRunner.askTimeOut();
         do {
-            GameRunner.runGame(p1, p2);
-        } while (GameRunner.askToPlayAgain());
-        GameRunner.dispose();
+            gameRunner.runGame(p1, p2);
+        } while (gameRunner.askToPlayAgain());
+        gameRunner.dispose();
     }
 }
--- src/main/java/me/ilinskiy/chess/game/GameRunner.java
+++ src/main/java/me/ilinskiy/chess/game/GameRunner.java
@@ -3,17 +3,19 @@ package me.ilinskiy.chess.game;
 import me.ilinskiy.chess.annotations.NotNull;
 import me.ilinskiy.chess.chessBoard.PieceColor;
 import me.ilinskiy.chess.ui.ChessPainter;
-import me.ilinskiy.chess.ui.JSwingChessPainter;
 
 /**
  * Author: Svyatoslav Ilinskiy
  * Date: 8/5/15.
  */
 public class GameRunner {
-    private static final ChessPainter painter = new JSwingChessPainter();
+    private final ChessPainter painter;
     public static final boolean DEBUG = false;
     public static int TIMEOUT_IN_SECONDS = 0; //if timeout is 0, the only limit is yourself
 
+    public GameRunner(ChessPainter p) {
+        painter = p;
+    }
 
     /**
      * Run the actual game with the given players
@@ -25,7 +27,7 @@ public class GameRunner {
      * @return the piece color of the winner or PieceColor.Empty if it's a draw
      */
     @NotNull
-    public static PieceColor runGame(@NotNull Player p1, @NotNull Player p2) {
+    public PieceColor runGame(@NotNull Player p1, @NotNull Player p2) {
         Game g;
         g = new Game(p1, p2, painter);
         while (!g.isGameOver()) {
@@ -46,14 +48,14 @@ public class GameRunner {
      *
      * @return true if play again
      */
-    public static boolean askToPlayAgain() {
+    public boolean askToPlayAgain() {
         return painter.askToPlayAgain();
     }
 
     /**
      * Ask the user the timeout they want to use. It sets the timeout according to the answer
      */
-    public static void askTimeOut() {
+    public void askTimeOut() {
         TIMEOUT_IN_SECONDS = painter.askTimeOut();
         if (TIMEOUT_IN_SECONDS < 0) {
             throw new RuntimeException("Timeout cannot be negative!");
@@ -63,7 +65,7 @@ public class GameRunner {
     /**
      * Disposes of the frame
      */
-    public static void dispose() {
+    public void dispose() {
         painter.dispose();
     }
 }
